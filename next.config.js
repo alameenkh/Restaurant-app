263a4cd6c90eb6ba964040be2e4b9588c958e72e
alameen <ameen.kh@naicoits.com>
2022-03-17 14:01:40 +0530

    Reverted export package json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://bigsloop.s3.amazonaws.com/images/restaurant/img'],
+  }
 }
 
 module.exports = nextConfig
